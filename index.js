b6c363bee385f69fa400b6b3e8977bc6e1c7e235
igor <igor.honhoff@gmail.com>
2020-11-23 18:06:01 +0200

    refactoring main vs renderer initialisation

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 const isRenderer = require( "is-electron-renderer" )
 
-const exp = {}
-
 if ( isRenderer ) {
-  exp.extensionPoints = require( "./lib/common/extension-manager" )
-  exp.activation = require( "./lib/common/activation-manager" )
-  exp.plugins = require( "./lib/renderer/plugin-facade")
-
-  const mock = require( "mock-require" )
-  exp.initEps = epsName => {
-    if ( epsName ) {    
-      mock( epsName, exp.extensionPoints.get() )
-    } 
-  }
+  module.exports = require( "./lib/renderer" )
 } else {
-  const init = require( "./lib/main/init" )
-  const useRenderer = require( "./lib/main/router" )
-  const store = require( "./lib/main/store" )
-  
-  exp.loadPlugins = init.loadPlugins
-  exp.installPlugin = init.installPlugin
-  exp.useRenderer = useRenderer
-  exp.register = {
-    getPlugin: store.getPlugin,
-    getAllPlugins: store.getAllPlugins,
-    getActivePlugins: store.getActivePlugins
-  }
-}
-
-module.exports = exp
+  module.exports = require( "./lib/main" )
+} 
