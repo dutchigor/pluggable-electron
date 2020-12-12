c539fa888f894db9866a0bfc9e8bf942447587f7
igor <igor.honhoff@gmail.com>
2020-12-12 12:58:45 +0200

    Added wiki as submodule

--- docs/genDocs.js
+++ docs/genDocs.js
@@ -3,19 +3,19 @@ const jsdoc2md = require( "jsdoc-to-markdown" ),
   { resolve, join } = require( "path" )
 
 const base = resolve( __dirname, '..' ),
-  wiki = 'wiki',
-  lib = 'lib'
+  wiki = join( base, 'docs', 'wiki' ),
+  lib = join( base, 'lib' )
 
 // Create API documentation
 const createDoc = async ( inFiles, tmpl, outFile ) => {
   try {
     // Prepare input data
-    const files = inFiles.map( glob => join( base, lib, glob ) )
+    const files = inFiles.map( glob => join( lib, glob ) )
     const template = await fs.readFile( join( __dirname, tmpl ), 'utf8' )
 
     // Generate documentation and write to file
     const md = await jsdoc2md.render( { files, template } )
-    await fs.writeFile( join( base, wiki, outFile ), md )      
+    await fs.writeFile( join( wiki, outFile ), md )      
   } catch ( error ) {
     console.log( error )
   }
@@ -25,12 +25,12 @@ const createDoc = async ( inFiles, tmpl, outFile ) => {
 createDoc(
   [ 'main/*.js' ],
   'main-API.hbs',
-  'main-API.md'
+  'Main-API.md'
 )
 
 // Generate renderer documentation
 createDoc(
   [ 'renderer/*.js', 'common/*.js' ],
   'renderer-API.hbs',
-  'renderer-API.md'
+  'Renderer-API.md'
 )
