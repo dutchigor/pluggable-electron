46b7532b01baee50178ebb1cda2105db0df701cd
igor <igor@flarehub.io>
2022-10-26 17:08:25 +0300

    Include confirmInstall in plugin manager docs

--- pluginMgr/globals.js
+++ pluginMgr/globals.js
@@ -5,10 +5,6 @@ export let pluginsPath = null
 
 /**
  * @private
- * @module globals
- **/
-
-/**
  * Set path to plugins directory and create the directory if it does not exist.
  * @param {string} plgPath path to plugins directory
  */
@@ -33,10 +29,25 @@ export function setPluginsPath(plgPath) {
 }
 
 /**
+* @private
  * Get the path to the plugins.json file.
  * @returns location of plugins.json
  */
 export function getPluginsFile() { return join(pluginsPath, 'plugins.json') }
+
+
+export let confirmInstall = function () {
+  return new Error(
+    'The facade.confirmInstall callback needs to be set in when initializing Pluggable Electron in the main process.'
+  )
+}
+
+/**
+ * @private
+ * Set callback to use as confirmInstall.
+ * @param {confirmInstall} cb Callback
+ */
+export function setConfirmInstall(cb) { confirmInstall = cb }
 
 /**
  * This function is executed when a plugin is installed to verify that the user indeed wants to install the plugin.
@@ -44,10 +55,3 @@ export function getPluginsFile() { return join(pluginsPath, 'plugins.json') }
  * @param {string} plg The specifier used to locate the package (from NPM or local file)
  * @returns {Promise<boolean>} Whether to proceed with the plugin installation
  */
-export let confirmInstall = function (plg) {
-  return new Error(
-    'The facade.confirmInstall callback needs to be set in when initializing Pluggable Electron in the main process.'
-  )
-}
-
-export function setConfirmInstall(cb) { confirmInstall = cb }
